09baf11cd25950b48f074b7097ce89941f379a83
dimpurr <dimpurr@live.com>
2017-06-19 21:19:29 +0800

    Remove TypeScript and move webpack to frontend.

---
+++ src/web/webpack.config.js
@@ -0,0 +1,20 @@
+// module.exports = {
+//     entry: './app.js',
+//     output: {
+//         filename: 'bundle.js'
+//     },
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.js$/,
+//                 exclude: /(node_modules|bower_components)/,
+//                 use: {
+//                     loader: 'babel-loader',
+//                     options: {
+//                         presets: ['env']
+//                     }
+//                 }
+//             }
+//         ]
+//     }
+// }
