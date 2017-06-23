b8c010fa11222fcf584486d15c2e1b17fac29fc3
Dring <dring13@yeah.net>
2017-06-24 00:14:19 +0800

    Update manage page

--- web/src/components/manage.jsx
+++ web/src/components/manage.jsx
@@ -14,36 +14,79 @@ class Manage extends React.Component {
   }
 }
 
-function ProjectList() {
-  function Project() {
+class ProjectList extends React.Component {
+  render () {
     return (
-      <li>
-        <div className='manage-list-left'>
-          <a className='proj-name' href={site_url + '/codemoe/hybond/'}>Hybond</a>
-          <span className='description'>A Magical Bond for Hybrid Developer</span>
-        </div>
-        <div className='manage-list-right'>
-          <button className='manage-list-edit'><i className='material-icons'>edit</i></button>
-          <button className='manage-list-delete'><i className='material-icons'>delete</i></button>
-        </div>
-        <div className='clearfix'></div>
-      </li>
+      <ul className='manage-list'>
+        <Project />
+        <Project />
+        <Project />
+        <Project />
+        <p className='manage-page'>
+          <div>
+            <button className='manage-page-btn'><i className='material-icons'>arrow_back</i></button>
+            <span className='manage-page-num'>1 <i>/</i> 5</span>
+            <button className='manage-page-btn'><i className='material-icons'>arrow_forward</i></button>
+          </div>
+        </p>
+      </ul>
     );
   }
+}
 
+function Project() {
   return (
-    <ul className='manage-list'>
-      <Project />
-      <Project />
-      <Project />
-      <Project />
-      <p className='manage-page'>
-        <button className='manage-page-btn'><i className='material-icons'>arrow_back</i></button>
-        <span className='manage-page-num-current'><b>1</b></span><span className='manage-page-num-all'><b>5</b></span>
-        <button className='manage-page-btn'><i className='material-icons'>arrow_forward</i></button>
-      </p>
-    </ul>
+    <li>
+      <div className='manage-list-left'>
+        <a className='proj-name' href={site_url + '/codemoe/hybond/'}>Hybond</a>
+        <span className='description'>A Magical Bond for Hybrid Developer</span>
+      </div>
+      <div className='manage-list-right'>
+        <button className='manage-list-edit'><i className='material-icons'>edit</i></button>
+        <button className='manage-list-delete'><i className='material-icons'>delete</i></button>
+      </div>
+      <div className='clearfix'></div>
+      <ManageAlert />
+    </li>
   );
+}
+
+class ManageAlert extends React.Component {
+  constructor () {
+    super();
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      value: '',
+      className: '',
+    };
+  }
+
+  handleChange (event) {
+    this.setState({value: event.target.value});
+    if (event.target.value != '') {
+      this.setState({className: 'have-content'});
+    } else {
+      this.setState({className: ''});
+    }
+  }
+
+  render () {
+    return (
+      <div className='manage-alert'>
+        <header>
+          <h3><i className='material-icons'>warning</i> 危险操作</h3>
+          <p>
+            您确定要删除项目 <span className='proj-name'>Hybond</span> 吗？这一操作不可逆，请谨慎操作。<br />
+            如果您确定要这么做，请输入项目名称（<span className='proj-name'>Hybond</span>）：
+          </p>
+          <button><i className='material-icons'>close</i></button>
+        </header>
+        <div className='manage-alert-input'><input type='text' className={this.state.className} onChange={this.handleChange} value={this.state.value} /><span className='manage-alert-alt'>项目名称</span></div><br />
+        <button>取消</button><button className='manage-alert-delete'>永久删除</button>
+        <div className='clearfix'></div>
+      </div>
+    );
+  }
 }
 
 export default Manage;
